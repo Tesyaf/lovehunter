d5190208c8b92a9e65944e62839af7ad0d067fb4
Tesyaf <abraralif3@gmail.com>
2023-11-29 21:14:53 +0700

    menambahkan ukuran screen, kode warna dan arah

--- Game.cpp
+++ Game.cpp
@@ -6,3 +6,28 @@
 #include <time.h>
 
 using namespace std;
+
+#define SCREEN_WIDTH 90
+#define SCREEN_HEIGHT 26
+#define WIN_WIDTH 70
+
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define PURPLE 5
+#define DARKYELLOW 6
+#define WHITE 7
+#define GREY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define PINK 13
+#define YELLOW 14
+#define LIGHTWHITE 15
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
